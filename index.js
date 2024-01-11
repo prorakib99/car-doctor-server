3d8e9bdeb6a65276ce6a02e18491b7683b030c39
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-11 19:35:51 +0600

    Implement Booking Service get data from client side and saved database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,10 +27,27 @@ async function run() {
         await client.connect();
 
         const servicesCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        // Services
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const options = { projection: { title: 1, price: 1, service_id: 1, img: 1 }, };
+            const result = await servicesCollection.findOne(query, options);
+            res.send(result)
+        })
+
+        // Booking
+        app.post('/bookings', async (req, res) => {
+            const newBooking = req.body;
+            const result = await bookingCollection.insertOne(newBooking);
             res.send(result)
         })
 
