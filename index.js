a2649d0dce1cab3240df5c63620b6f24b421a0bc
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-12 01:44:49 +0600

    Implement Booking Status update from client site via Patch method

--- index.js
+++ index.js
@@ -61,6 +61,19 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateBooking = req.body;
+            const filter = { _id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                  status: updateBooking.status
+                },
+              };
+            const result = await bookingCollection.updateOne(filter, updateDoc);  
+            res.send(result)
+        })
+
         app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id)};
